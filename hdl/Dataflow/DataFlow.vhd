cbd6a876c4860c64a47ce347a429f77b1f5ba03d
Sid <miranda.jp2017@gmail.com>
2022-06-20 18:17:52 -0300

    Instruções do Tipo D adaptadas ao padrão ARM

--- hdl/Dataflow/DataFlow.vhd
+++ hdl/Dataflow/DataFlow.vhd
@@ -1,7 +1,7 @@
 
 -------------------------------------------------------
 --! @file DataFlow.vhd
---! @brief ALU do polilegv8
+--! @brief Dataflow do polilegv8
 --! @author Joao Pedro Cabral Miranda (miranda.jp@usp.br)
 --! @date 2022-05-26
 -------------------------------------------------------
@@ -307,12 +307,12 @@ begin
     -- Data Memory
     data_memory_address <= alu_out;
     write_data_memory_mux: component mux4x1 generic map(word_size) port map(write_data_memory_b, write_data_memory_h, write_data_memory_w, read_data_b, data_memory_src, write_data);
-    write_data_memory_b <= bit_vector(resize(signed(read_data_b(word_size/8 - 1 downto 0)), word_size));
-    write_data_memory_h <= bit_vector(resize(signed(read_data_b(word_size/4 - 1 downto 0)), word_size));
+    write_data_memory_b <= bit_vector(resize(unsigned(read_data_b(word_size/8 - 1 downto 0)), word_size));
+    write_data_memory_h <= bit_vector(resize(unsigned(read_data_b(word_size/4 - 1 downto 0)), word_size));
     write_data_memory_w <= bit_vector(resize(signed(read_data_b(word_size/2 - 1 downto 0)), word_size));
     read_data_memory_mux: component mux4x1 generic map(word_size) port map(read_data_memory_b, read_data_memory_h, read_data_memory_w, read_data, data_memory_src, read_data_mux_out);
-    read_data_memory_b <= bit_vector(resize(signed(read_data(word_size/8 - 1 downto 0)), word_size));
-    read_data_memory_h <= bit_vector(resize(signed(read_data(word_size/4 - 1 downto 0)), word_size));
-    read_data_memory_w <= bit_vector(resize(signed(read_data(word_size/2 - 1 downto 0)), word_size));
+    read_data_memory_b <= bit_vector(resize(unsigned(read_data(word_size/8 - 1 downto 0)), word_size));
+    read_data_memory_h <= bit_vector(resize(unsigned(read_data(word_size/4 - 1 downto 0)), word_size));
+    read_data_memory_w <= bit_vector(resize(unsigned(read_data(word_size/2 - 1 downto 0)), word_size));
 
 end architecture structural;
