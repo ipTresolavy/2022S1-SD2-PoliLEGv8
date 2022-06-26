669d50ec83a7902c51f39ee1d596986166ed21e4
Joao P Selva <jpselva@usp.br>
2022-06-26 00:46:33 -0300

    finaliza instruções B, B.cond, CBZ e CBNZ na UC

--- hdl/Control_Unit/control_unit.vhd
+++ hdl/Control_Unit/control_unit.vhd
@@ -28,6 +28,7 @@ entity control_unit is
         overflow_r: in bit;
         negative_r: in bit;
         stxr_try_in: in bit;
+        flags_cond_sel: in bit_vector(3 downto 0);
 
         -- To Dataflow
             -- MOV's signals
@@ -44,10 +45,16 @@ entity control_unit is
         alu_pc_b_src: out bit;
             -- PC's signals
         pc_src: out bit;
+        pc_branch_src: out bit;
         pc_enable: out bit;
         	-- monitor's signals
         monitor_enable: out bit;
-
+            -- reg file
+        read_register_a_src: out bit;
+        read_register_b_src: out bit;
+        write_register_src: out bit_vector(1 downto 0);
+        write_register_data_src: out bit_vector(1 downto 0);
+        write_register_enable: out bit;
         -- Instruction memory
         instruction_mem_enable : out bit;
         instruction_mem_busy   : in bit;
@@ -63,37 +70,29 @@ architecture control_unit_beh of control_unit is
 
     type state_type is (fetch_decode, stxr_execute, branch_relative);
 
-    signal next_state, current_state : state_type := fetch;
-    signal flags_mux_out, cbz, cbnz, uncond_branch : bit;
-    signal flags_mux_sel : bit_vector(3 downto 0);
+    signal next_state, current_state : state_type := fetch_decode;
+    signal flags_mux_out, flags_mux_final, cbz, cbnz, b_flags, uncond_branch : bit;
 begin
-        pc_src <= (b_cond and flags_mux_out) or 
+        pc_src <= (b_flags and flags_mux_out) or 
                   (zero and cbz) or 
-                  ((not zero) and cbnz) or
-                  uncond_branch;
+                  ((not zero) and cbnz);
 
-        with flags_mux_sel select flags_mux_out <=
-            negative_r when "0000", 
-            not negative_r when "0001", 
-            overflow_r when "0010", 
-            not overflow_r when "0011", 
-            zero_r when "0100", 
-            not zero_r when "0101", 
-            negative_r xor overflow_r when "0110", 
-            zero_r or (negative_r xor overflow_r) when "0111", 
-            not zero_r and not (negative_r xor overflow_r) when "1000", 
-            not (negative_r xor overflow_r) when "1001", 
-            zero_r when "1010", 
-            not zero_r when "1011", 
-            not carry_out_r when "1100", 
-            not (not zero_r and carry_out_r) when "1101", 
-            not zero_r and carry_out_r when "1110", 
-            carry_out_r when "1111";
+        with flags_cond_sel(3 downto 1) select flags_mux_out <=
+            zero_r when "000", 
+            carry_out_r when "001",
+            negative_r when "010", 
+            overflow_r when "011", 
+            not zero_r and carry_out_r when "100", 
+            negative_r xor overflow_r when "101", 
+            not zero_r and not (negative_r xor overflow_r) when "110",
+            '1' when others;
+
+        flags_mux_final <= (flags_mux_out xor (flags_cond_sel(0) and not (flags_cond_sel(3) and flags_cond_sel(2) and flags_cond_sel(1))));
 
         change_of_state: process(clock, reset) is
             begin
                 if reset = '1' then
-                    current_state <= fetch;
+                    current_state <= fetch_decode;
                 elsif rising_edge(clock) then
                     current_state <= next_state;
                 end if;
@@ -109,11 +108,14 @@ begin
                 mul_div_src <= '0';
                 mul_div_enable <= '0';
                 alu_pc_b_src <= '0';
-                pc_src <= '0';
                 pc_enable <= '0';
                 monitor_enable <= '0';
                 instruction_mem_enable <= '0';
                 data_mem_enable <= '0';
+                uncond_branch <= '0';
+                b_flags <= '0';                
+                cbz <= '0';
+                cbnz <= '0';
             end procedure;
 
             procedure wait_for_data_mem(we:boolean) is
@@ -142,15 +144,14 @@ begin
                 reset_control_signals;
 
                 case current_state is
-                    when fetch =>
+                    when fetch_decode =>
                         instruction_mem_enable <= '1';
                         wait until instruction_mem_busy = '1';
                         wait until instruction_mem_busy = '0';
                         instruction_mem_enable <= '0';
-                        next_state <= decode_execute;
 
                         -- colocar BL antes desse
-                        if (opcode(10 downto 5) = "000101" or opcode(7 downto 4) = "1010")
+                        if (opcode(10 downto 5) = "000101" or opcode(7 downto 4) = "1010") then
                             next_state <= branch_relative;
                         end if;
 
@@ -158,8 +159,20 @@ begin
                         read_register_b_src <= '1';
                         alu_control <= "011";
                         pc_enable <= '1';
+                        pc_branch_src <= '0';
 
-                        if 
+                        if opcode(10 downto 9) = "00" then
+                            uncond_branch <= '1';
+                        elsif opcode(10 downto 9) = "01" then
+                            b_flags <= '1';
+                        elsif opcode(10 downto 9) = "10" then 
+                            if opcode(4) = '0' then
+                                cbz <= '1';
+                            else
+                                cbnz <= '1';
+                            end if;
+                        end if;
+                            
                         
                     when stxr_execute =>
 
